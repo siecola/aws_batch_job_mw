db5224a9d2cb7c3016cfd356dc829e3471342bfe
siecola <siecola@gmail.com>
2019-05-07 00:19:52 -0300

    Primary for DynamoDB beans

--- src/main/java/br/com/siecola/aws_batch_job_mw/config/DynamoDBConfig.java
+++ src/main/java/br/com/siecola/aws_batch_job_mw/config/DynamoDBConfig.java
@@ -10,23 +10,27 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @EnableDynamoDBRepositories(basePackageClasses = JobRepository.class)
 public class DynamoDBConfig {
 
     @Bean
+    @Primary
     public DynamoDBMapperConfig dynamoDBMapperConfig() {
         return DynamoDBMapperConfig.DEFAULT;
     }
 
     @Bean
+    @Primary
     public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB,
                                          DynamoDBMapperConfig config) {
         return new DynamoDBMapper(amazonDynamoDB, config);
     }
 
     @Bean
+    @Primary
     public AmazonDynamoDB amazonDynamoDB() {
         return AmazonDynamoDBClientBuilder.standard()
                 .withCredentials(new DefaultAWSCredentialsProviderChain())
